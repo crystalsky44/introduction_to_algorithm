f7bd9c6485f401f0732cc7a1bb19f2933bff0deb
Yoshishige Unose <966.krr@gmail.com>
2025-09-11 18:11:09 +0900

    add function: array_and_value_generator, insertion_sort \nchange the name of array and value generator

--- chapter2_insertion_sort/src/main.rs
+++ chapter2_insertion_sort/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+
+    let before_sort: [i32] = generating_array_to_sort();
+    println!("before sort: {before_sort}");
+
+    let after_sort = insert_sort(before_sort, items_to_sort);
+    println!("after sort: {after_sort}");
+}
+
+fn generating_array_to_sort() -> [i32] {
+    // some code
+
+}
+
+fn insert_sort(array: [i32], items_to_sort: i32) -> {
+    for items in (0..items_to_sort) {
+
 }
