9b104bd7a110088a9dabc8f645e1243a0a50d093
Yoshishige Unose <966.krr@gmail.com>
2025-09-17 13:05:18 +0900

    add fn print_array, insert_sort logic

--- chapter2_insertion_sort/src/main.rs
+++ chapter2_insertion_sort/src/main.rs
@@ -1,18 +1,35 @@
 fn main() {
 
-    let before_sort: [i32] = generating_array_to_sort();
-    println!("before sort: {before_sort}");
+    let mut array_to_sort = [4, 5, 2, 6, 7, 14, 45, 64, 2, 9, 17, 97];
+    print!("before sort: ");
+    print_array(&mut array_to_sort);
 
-    let after_sort = insert_sort(before_sort, items_to_sort);
-    println!("after sort: {after_sort}");
+    insert_sort(&mut array_to_sort);
+
+    print!("\nafter sort: ");
+    print_array(&mut array_to_sort);
 }
 
-fn generating_array_to_sort() -> [i32] {
-    // some code
+fn insert_sort(array: &mut [i32]) -> () { 
+    print!("in fn_insert_sort: ");
 
+    
+    for item in 0..array.len() {
+        let key = item;
+        print!("{key} ");
+        /*
+        if key < array[array_position - 1] {
+            array[array_position] = array[array_position - 1];
+            array[array_position - 1] = key;
+        }
+        */
+    }
 }
 
-fn insert_sort(array: [i32], items_to_sort: i32) -> {
-    for items in (0..items_to_sort) {
+fn print_array(array: &mut [i32]) -> () {
+    for item in &*array {
+        print!("{item} ");
+    }
 
+    println!();
 }
